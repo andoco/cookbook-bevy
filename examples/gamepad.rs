06b1167e86902a5e1af1a1ef0d56c1a597ad96ef
Andrew O'Connor <andrew.oconnor@gmail.com>
2022-12-02 08:03:39 +0000

    Rotate and move forward/back

--- examples/gamepad.rs
+++ examples/gamepad.rs
@@ -6,18 +6,24 @@ fn main() {
         .add_startup_system(setup)
         .add_system(handle_connected)
         .add_system(handle_event)
+        .add_system(move_camera)
         .run();
 }
+
+#[derive(Component)]
+struct Speed(Vec2);
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0., 0.5, 0.),
-        ..default()
-    });
+    commands
+        .spawn(Camera3dBundle {
+            transform: Transform::from_xyz(0., 0.5, 0.),
+            ..default()
+        })
+        .insert(Speed(Vec2::ZERO));
 
     // ground plane
     commands.spawn(PbrBundle {
@@ -45,21 +51,27 @@ fn handle_connected(gamepads: Res<Gamepads>) {
 
 fn handle_event(
     mut events: EventReader<GamepadEvent>,
-    mut camera_query: Query<&mut Transform, With<Camera>>,
+    mut camera_query: Query<&mut Speed, With<Camera>>,
 ) {
     for event in events.iter() {
         info!("{:?}", event);
 
         match event.event_type {
             GamepadEventType::AxisChanged(GamepadAxisType::LeftStickX, value) => {
-                let angle = 10. * value.to_radians();
-                camera_query.single_mut().rotate_y(angle);
+                camera_query.single_mut().0.x = value;
             }
             GamepadEventType::AxisChanged(GamepadAxisType::LeftStickY, value) => {
-                let angle = 10. * value.to_radians();
-                camera_query.single_mut().rotate_x(angle);
+                camera_query.single_mut().0.y = -value;
             }
             _ => {}
         }
+    }
+}
+
+fn move_camera(mut camera_query: Query<(&Speed, &mut Transform), With<Camera>>) {
+    if let Ok((speed, mut tx)) = camera_query.get_single_mut() {
+        tx.rotate_y(-speed.0.x.to_radians());
+        let dir = tx.local_z();
+        tx.translation += dir * speed.0.y;
     }
 }
